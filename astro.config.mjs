318f458e424a23f24ae5040f4c46a4055523bbad
Yakubu <jnrkhld@gmail.com>
2023-10-21 07:56:15 +0000

    adding site maps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()]
+  site: 'https://codingkickstart.netlify.app',
+  integrations: [svelte(), tailwind(), sitemap()]
 });
